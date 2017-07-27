55cda09ffc09de266bd9a8046c5353ab6672e886
stalin <allive@ukr.net>
2017-07-27 19:17:53 +0300

    Add function skillet for chose country dialog;

--- webogram2/src/app/components/sign-in/sign-in.component.ts
+++ webogram2/src/app/components/sign-in/sign-in.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+        this.http.get('/api/items').subscribe(data => {
+            console.log(data);
+        });
+
+
+    }
 
 }
